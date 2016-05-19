3977900a1751d84e07dde65cc596bd11382bfc93
Hugo TOSTAIN <ugotost1@gmail.com>
2016-05-19 17:53:48 +0200

    Ajout table user, password et recettes en base de données

---
+++ database/migrations/2016_05_19_154741_create_recettes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecettesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recettes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description');
+            $table->string('category');
+            $table->text('recette');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('recettes');
+    }
+}
