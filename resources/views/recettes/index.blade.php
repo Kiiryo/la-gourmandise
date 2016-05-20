d91ab7a66baf573d647815188090e14d4affc345
Hugo TOSTAIN <ugotost1@gmail.com>
2016-05-20 11:50:10 +0200

    modif page liste

--- resources/views/recettes/index.blade.php
+++ resources/views/recettes/index.blade.php
@@ -5,13 +5,13 @@
         <div class="row">
             @include('errors.message')
             @foreach($list as $recette)
-                <div class=" col-md-6">
-                    <div class="col-md-4">
+                <div class=" col-md-6 col-sm-12 col-xs-12">
+                    <div class="col-md-5 col-xs-5">
                         <img src="{{asset('img/crepe.JPG')}}" alt="" class="img_recette">
                     </div>
-                    <div class="col-md-8">
+                    <div class="col-md-7 col-xs-7">
                         <a href="{{route('recette.show', $recette->id)}}">
-                            <h2>{{$recette->id}}. {{$recette->title}}</h2>
+                            <h3>{{$recette->id}}. {{$recette->title}}</h3>
                         </a>
                         <p class="auteur">Par :{{$recette->username}}</p>
                         <h4>{{$recette->description}}</h4>
