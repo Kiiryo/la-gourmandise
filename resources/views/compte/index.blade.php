c7b6b880882d064c52b1ba3d6836263554cfd0cd
Hugo TOSTAIN <ugotost1@gmail.com>
2016-06-10 14:10:07 +0200

    modif css + ajout partie compte

--- resources/views/compte/index.blade.php
+++ resources/views/compte/index.blade.php
@@ -3,8 +3,17 @@
 @section('content')
     <div class="container margin">
         <div class="row">
+        @if(Auth::check())
             @include('errors.message')
-            <h1>Ceci est la page compte utilisateur</h1>
+            <h2 class="titre-compte">Vous êtes connecté en temps que {{ Auth::user()->name }} !</h2>
+            <div class="deco">
+                <a href="{{ url('/logout') }}"><button class="connexion btn btn-danger"><i class="fa fa-sign-out fa-5" aria-hidden="true"></i> Déconnexion</button></a>
+            </div>
+
+        @else
+            <h1>Vous n'êtes pas connecté, veuillez vous connecter !</h1>
+            <a href="{{ url('/login') }}"><button class="connexion btn btn-primary"><i class="fa fa-sign-in fa-5" aria-hidden="true"></i> Connexion</button></a>
+        @endif
         </div>
     </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,7 +75,7 @@
                         <li><a href="{{ url('/login') }}"><i class="fa fa-sign-in fa-5" aria-hidden="true"></i><br> Connexion</a></li>
                         <li><a href="{{ url('/register') }}"><i class="fa fa-pencil-square-o fa-5" aria-hidden="true"></i><br> Inscription</a></li>
                     @else
-                        <li class="dropdown">
+                        <!--<li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
@@ -83,7 +83,7 @@
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Déconnexion</a></li>
                             </ul>
-                        </li>
+                        </li> -->
                     @endif
                 </ul>
 
