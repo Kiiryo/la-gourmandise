56bebe93fd0ece72f54f54b62726de004572c97a
Julien XU <xu1996@hotmail.fr>
2016-06-14 12:32:01 +0200

    Style page Detail recettes !

--- resources/views/recettes/show.blade.php
+++ resources/views/recettes/show.blade.php
@@ -1,30 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container margin">
+    <div class="container-fluid margin">
         <div class="row">
             @include('errors.message')
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">{{$recette->title}}</div>
-                    <div class="panel-body">
-                        <p> Auteur: {{ $recette->username }}</p>
-                        <p><b>Descriptif: </b>{{$recette->description}}</p>
-                        <p><b>Categorie: </b>{{$recette->category}}</p>
-                        <p><b>Etapes de la recette: </b>{{$recette->recette}}</p>
-                        <p><b>Difficulté de la recette: </b>{{$recette->difficulte}}</p>
-
-                        @if(Auth::check() && (Auth::user()->id == $recette->user_id || Auth::user()->isAdmin))
-                        <a href="{{ route('recette.edit', $recette->id)}}" class="btn btn-success btn-line btn-rect">
-                            <i class="fa fa-pencil"></i> Editer
-                        </a>
-                        {!! Form::model($recette, array('route' => array('recette.destroy', $recette->id), 'method' => 'DELETE')) !!}
-                        {!! Form::submit('Supprimer', ['class' => 'btn btn-danger']) !!}
-                        {!! Form::close() !!}
-
-                        @endif
+            <div class="col-md-12 col-xs-12 img-slide">
+                <img src="{{asset('img/recet_img/'.$recette->id.$recette->image)}}" alt="" class="img-detail">
+            </div>
+            <div class="col-md-12 col-xs-12 descrip-titre">
+                   <h3 class="titre-detail text-center">
+                       {{$recette->title}}
+                   </h3>
+            </div>
+            <div class="col-md-12 col-xs-12 descrip-all">
+                <div class="icon">
+                    <div class="col-xs-4 text-center border-right">
+                        <i class="fa fa-cutlery fa-detail" aria-hidden="true"></i>
+                        <p>{{$recette->category}}</p>
+                    </div>
+                    <div class="col-xs-4 text-center border-right">
+                        <i class="fa fa-line-chart fa-detail" aria-hidden="true"></i>
+                        <p>{{$recette->difficulte}}</p>
+                    </div>
+                    <div class="col-xs-4 text-center ">
+                        <i class="fa fa-user fa-detail " aria-hidden="true"></i>
+                        <p>{{ $recette->username }}</p>
                     </div>
                 </div>
+
+                   <p><b>Description: </b><br>{{$recette->description}}</p>
+
+                   <p><b>Etapes de la recette: </b><br>{{$recette->recette}}</p>
+
+
+                   @if(Auth::check() && (Auth::user()->id == $recette->user_id || Auth::user()->isAdmin))
+                   <a href="{{ route('recette.edit', $recette->id)}}" class="btn btn-success btn-line btn-rect">
+                       <i class="fa fa-pencil"></i> Editer
+                   </a>
+                   {!! Form::model($recette, array('route' => array('recette.destroy', $recette->id), 'method' => 'DELETE')) !!}
+                   {!! Form::submit('Supprimer', ['class' => 'btn btn-danger']) !!}
+                   {!! Form::close() !!}
+
+                   @endif
             </div>
 
         </div>
